256be5d6c35f741a746bd0f478305dfcc248bf00
Carlos Brayan Rámila Chorens <carlos.bramila98@gmail.com>
2023-12-27 16:52:09 -0500

    reprobando 1

--- src/views/Nasa.vue
+++ src/views/Nasa.vue
@@ -1,7 +1,6 @@
 <script setup>
 import {useStore} from "vuex";
 import {computed, onMounted, reactive, ref} from "vue";
-import {createClient} from '@supabase/supabase-js'
 
 
 const store = useStore()
